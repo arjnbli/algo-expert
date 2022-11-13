28eaf9e85741b9742d8ee1b4155ab7b21ee6e283
Arjun Bali <37790028+arjnbli@users.noreply.github.com>
2022-11-13 10:40:57 -0800

    breadthFirstSearch.py

---
+++ graphs/breadthFirstSearch.py
@@ -0,0 +1,28 @@
+# Do not edit the class below except
+# for the breadthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    # O(v + e) time | O(v) space
+    # v is the number of vertices in the graph
+    # e is the number of edges in the graph
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        visited = set()
+        queue = [self]
+        while queue:
+            vertex = queue.pop(0)
+            array.append(vertex.name)
+            visited.add(vertex)
+            for child in vertex.children:
+                if child not in visited:
+                    queue.append(child)
+        return array
