c21ba03a29df0385e28f7e81f748ae162df28f6c
Arjun Bali <37790028+arjnbli@users.noreply.github.com>
2022-11-13 10:51:59 -0800

    Write bfs implementation for scenario in which adjacency list is not used

---
+++ graphs/traversalImplementations/bfs1.py
@@ -0,0 +1,22 @@
+class Vertex:
+    def __init__(self, value, adjacent=None):
+        self.value = value
+        self.adjacent = [] if adjacent is None else adjacent
+
+def visit(vertex):
+    pass
+
+# O(v + e) time | O(v + e) space
+# v is the number of vertices in the graph
+# e is the number of edges in the graph 
+def bfs(startVertex):
+    visited = set()
+    queue = [startVertex]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            visit(vertex)
+            for adjacentVertex in vertex.adjacent:
+                queue.append(adjacentVertex)
+    return 
